59eb3c04664e515cce532bff15856f844255829c
Amelia A <maantonib@yahoo.com.ar>
2025-09-28 18:46:00 -0300

    chore(pets): remove endpoint description comments

--- src/routes/mascotas.routes.js
+++ src/routes/mascotas.routes.js
@@ -9,19 +9,11 @@ import {
 
 const router = Router();
 
-// Obtener mascotas
+
 router.get("/", obtenerMascotasController);
-
-// Obtener una sola mascota por ID
 router.get("/:id", obtenerMascotaPorIdController);
-
-// Crear una mascota
 router.post("/", crearMascotaController);
-
-// Editar una mascota
 router.put("/:id", editarMascotaController);
-
-// Eliminar una mascota
 router.delete("/:id", eliminarMascotaController);
 
 export default router;
